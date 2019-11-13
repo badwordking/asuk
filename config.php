fc24424581b5542f503dc6084bdeaa980befdbeb
badwordking <51959848+badwordking@users.noreply.github.com>
2019-11-13 19:25:21 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$refresh_token = "AEu4IL3YQcY6Aiemg8zCq978ec38PSTfcHyddA1CXHd4VCwoO7oJqTAeCN-IZrwHfV0QQ_I8z2DN9o4Lszj49y44-rGGrjSZtO-SZW9dP421gmeK7Iknwza-0PlT8gLBRDRUz4uNg0aqvruNA3JtyBaG_ki5c9pHCS0cbpagjIZR3Tp1rVBM8rNuPbnvdrVJN_mWPr1Kzu0";
+
+$email_coinbase = "nsa5678@gmail.com";
+
+$target_withdraw = 445000;
+
+$version = "13";
